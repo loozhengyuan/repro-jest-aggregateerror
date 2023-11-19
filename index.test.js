9806692cbfdaa9c3bfeef9844c37f414d39dbd00
Loo Zheng Yuan <loozhengyuan@gmail.com>
2023-11-19 13:20:26 +0800

    test: catch and log error for debugging

--- index.test.js
+++ index.test.js
@@ -1,5 +1,10 @@
 const axios = require("axios")
 
 test('repro', async () => {
-    await axios.get('xxx')
+    try {
+        await axios.get('http://localhost:8080')
+    } catch (err) {
+        console.log(err)
+        throw err
+    }
 })
